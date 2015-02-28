d79c785c7842c4d96757c35bc0febb75a9145efd
hiroyuki nagata <newserver002@gmail.com>
2015-02-28 22:53:02 +0900

    Reimplement network settingpanel. Eject GUI information code from C++ source code

--- src/datatype.hpp
+++ src/datatype.hpp
@@ -45,6 +45,8 @@
 #define XROSSBOARD_DOWNLOADSITE wxT("http://hiroyuki-nagata.github.io/XrossBoard/")
 /** XrossBoardの作業用隠しフォルダ名 */
 #define XROSSBOARD_DIR          wxT(".xb")
+/** デフォルトの板一覧取得URL */
+#define DEFAULT_BOARDLIST_URL   wxT("http://kita.jikkyo.org/cbm/cbm.cgi/20.p0.m0.jb.vs.op.sc.nb.bb/-all/bbsmenu.html")
 /** コンフィグファイル名 */
 #define APP_CONFIG_FILE         wxT("xrossboard.env")
 /** クッキーの設定ファイル */
